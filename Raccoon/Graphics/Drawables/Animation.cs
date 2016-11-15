80d64d9787e32a7c732ab0253d0ce58cd1143005
Lucas Miranda <miranda.a.lucas@gmail.com>
2016-11-15 00:00:01 -0200

    Animation update

--- Raccoon/Graphics/Drawables/Animation.cs
+++ Raccoon/Graphics/Drawables/Animation.cs
@@ -14,13 +14,14 @@ namespace Raccoon.Graphics {
         #region Private Members
 
         private int _columns, _rows;
+        private Dictionary<KeyType, Track> _tracks;
 
         #endregion Private Members
 
         #region Constructors
 
         public Animation() {
-            Tracks = new Dictionary<KeyType, Track>();
+            _tracks = new Dictionary<KeyType, Track>();
         }
 
         public Animation(string filename, Size frameSize) : this() {
@@ -61,122 +62,145 @@ namespace Raccoon.Graphics {
 
         #region Public Properties
 
-        public Dictionary<KeyType, Track> Tracks { get; private set; }
-        public bool Playing { get; private set; }
+        public bool IsPlaying { get; private set; }
         public KeyType CurrentKey { get; private set; }
         public Track CurrentTrack { get; private set; }
         public int ElapsedTime { get; private set; }
+
+        public Track this[KeyType key] {
+            get {
+                return _tracks[key];
+            }
+        }
 
         #endregion Public Properties
 
         #region Public Methods
 
         public override void Update(int delta) {
-            if (!Playing)
+            if (!IsPlaying) {
                 return;
+            }
 
             ElapsedTime += delta;
-            if (ElapsedTime >= CurrentTrack.Duration) {
-                ElapsedTime = 0;
-                if (CurrentTrack.CurrentFrameIndex + 1 < CurrentTrack.Frames.Length) {
-                    CurrentTrack.CurrentFrameIndex++;
-                    UpdateClippingRegion();
-                } else if (CurrentTrack.IsLooping) {
-                    CurrentTrack.CurrentFrameIndex = 0;
-                    UpdateClippingRegion();
-                } else {
+            if (ElapsedTime >= CurrentTrack.CurrentFrameDuration) {
+                ElapsedTime -= CurrentTrack.CurrentFrameDuration;
+                CurrentTrack.NextFrame();
+                if (CurrentTrack.HasEnded) {
                     Pause();
+                } else {
+                    UpdateClippingRegion();
                 }
             }
         }
 
         public void Play(KeyType key, bool forceReset = true) {
-            Playing = true;
+            IsPlaying = true;
             if (CurrentTrack == null || !CurrentKey.Equals(key)) {
                 CurrentKey = key;
-                CurrentTrack = Tracks[CurrentKey];
-                CurrentTrack.CurrentFrameIndex = 0;
+                CurrentTrack = _tracks[CurrentKey];
+                CurrentTrack.Reset();
                 UpdateClippingRegion();
             } else if (forceReset) {
-                CurrentTrack.CurrentFrameIndex = 0;
+                CurrentTrack.Reset();
                 UpdateClippingRegion();
             }
         }
 
         public void Pause() {
-            Playing = false;
+            IsPlaying = false;
         }
 
         public void Stop() {
             Pause();
-            CurrentTrack.CurrentFrameIndex = 0;
+            CurrentTrack.Reset();
             UpdateClippingRegion();
         }
 
         public void Add(KeyType key, string frames, string durations) {
+            if (frames.IsEmpty()) throw new ArgumentException("Value is empty", "frames");
+            if (durations.IsEmpty()) throw new ArgumentException("Value is empty", "durations");
+
             List<int> durationList = new List<int>();
             foreach (Match m in DurationRegex.Matches(durations)) {
                 durationList.Add(int.Parse(m.Groups[1].Value));
             }
 
-            if (durationList.Count == 0) {
-                return;
-            }
+            if (durationList.Count == 0) throw new ArgumentException("Wrong value format", "durations");
 
             List<int> frameList = new List<int>();
-            int captureID = 0;
+            int frameIndex = 0;
             foreach (Match m in FrameRegex.Matches(frames)) {
-                if (m.Groups[3].Length > 0) {
-                    frameList.Add(int.Parse(m.Groups[3].Value));
-                    if (captureID == durationList.Count)
+                string frameId = m.Groups[3].Value;
+                if (frameId.Length > 0) {
+                    frameList.Add(int.Parse(frameId));
+                    if (frameIndex >= durationList.Count) {
                         durationList.Add(durationList[durationList.Count - 1]);
-                    captureID++;
+                    }
+
+                    frameIndex++;
                 } else {
-                    int d = durationList[captureID];
-                    durationList.RemoveAt(captureID);
-                    for (int i = int.Parse(m.Groups[1].Value); i <= int.Parse(m.Groups[2].Value); i++) {
+                    int d = durationList[frameIndex], startRange = int.Parse(m.Groups[1].Value), endRange = int.Parse(m.Groups[2].Value);
+                    durationList.RemoveAt(frameIndex);
+                    for (int i = startRange; i <= endRange; i++) {
                         frameList.Add(i);
-                        durationList.Insert(captureID, d);
-                        captureID++;
+                        durationList.Insert(frameIndex, d);
+                        frameIndex++;
                     }
                 }
             }
 
-            if (frameList.Count == 0) {
-                return;
+            if (frameList.Count == 0) throw new ArgumentException("Wrong value format", "frames");
+
+            if (durationList.Count > frameList.Count) {
+                durationList.RemoveRange(frameList.Count - 1, durationList.Count - frameList.Count);
             }
 
-            Tracks.Add(key, new Track(frameList.ToArray(), durationList.ToArray()));
+            _tracks.Add(key, new Track(frameList.ToArray(), durationList.ToArray()));
         }
 
         public void Add(KeyType key, string frames, int duration) {
+            if (frames.IsEmpty()) throw new ArgumentException("Value is empty", "frames");
+            if (duration < 0) throw new ArgumentException("Value is invalid, must be greater or equal zero", "durations");
+
             List<int> frameList = new List<int>();
             foreach (Match m in FrameRegex.Matches(frames)) {
-                if (m.Groups[3].Length > 0) {
-                    frameList.Add(int.Parse(m.Groups[3].Value));
+                string frameId = m.Groups[3].Value;
+                if (frameId.Length > 0) {
+                    frameList.Add(int.Parse(frameId));
                 } else {
-                    for (int i = int.Parse(m.Groups[1].Value); i <= int.Parse(m.Groups[2].Value); i++)
+                    int startRange = int.Parse(m.Groups[1].Value), endRange = int.Parse(m.Groups[2].Value);
+                    for (int i = startRange; i <= endRange; i++) {
                         frameList.Add(i);
+                    }
                 }
             }
+
+            if (frameList.Count == 0) throw new ArgumentException("Wrong value format", "frames");
 
             int[] durations = new int[frameList.Count];
             for (int i = 0; i < frameList.Count; i++) {
                 durations.SetValue(duration, i);
             }
 
-            Tracks.Add(key, new Track(frameList.ToArray(), durations));
+            _tracks.Add(key, new Track(frameList.ToArray(), durations));
         }
 
         public void Add(KeyType key, ICollection<int> frames, ICollection<int> durations) {
+            if (frames.Count == 0) throw new ArgumentException("Value is empty", "frames");
+            if (durations.Count == 0) throw new ArgumentException("Value is empty", "durations");
+
             int[] frameList = new int[frames.Count];
             frames.CopyTo(frameList, 0);
             int[] durationList = new int[durations.Count];
             durations.CopyTo(durationList, 0);
-            Tracks.Add(key, new Track(frameList, durationList));
+            _tracks.Add(key, new Track(frameList, durationList));
         }
 
         public void Add(KeyType key, ICollection<int> frames, int duration) {
+            if (frames.Count == 0) throw new ArgumentException("Value is empty", "frames");
+            if (duration < 0) throw new ArgumentException("Value is invalid, must be greater or equal zero", "durations");
+
             int[] frameList = new int[frames.Count];
             frames.CopyTo(frameList, 0);
 
@@ -185,7 +209,7 @@ namespace Raccoon.Graphics {
                 durations.SetValue(duration, i);
             }
 
-            Tracks.Add(key, new Track(frameList, durations));
+            _tracks.Add(key, new Track(frameList, durations));
         }
 
         #endregion Public Methods
@@ -211,30 +235,104 @@ namespace Raccoon.Graphics {
         #region Public Class Track
 
         public class Track {
-            public Track() {
-                IsLooping = true;
-            }
+            private event Action _onEnd, _onChangeFrame;
+            private int _currentFrameIndex;
 
-            public Track(int[] frames, int[] duration) : this() {
+            public Track(int[] frames, int[] duration) {
                 Frames = frames;
                 Durations = duration;
             }
 
             public int[] Frames { get; private set; }
             public int[] Durations { get; private set; }
-            public int Duration { get { return Durations[CurrentFrameIndex]; } }
-            //public int ElapsedTime { get; set; } // maybe I don't need to save time on each Track
-            public int CurrentFrameIndex { get; set; }
-            public bool IsLooping { get; set; }
+            public int CurrentFrameIndex { get { return _currentFrameIndex; } set { _currentFrameIndex = Math.Clamp(value, 0, Frames.Length - 1); } }
+            public int CurrentFrameDuration { get { return Durations[CurrentFrameIndex]; } }
+            public int CurrentSpriteID { get { return Frames[CurrentFrameIndex]; } }
+            public int RepeatTimes { get; set; }
+            public int TimesPlayed { get; private set; }
+            public bool HasEnded { get; private set; }
+            public bool IsLooping { get { return RepeatTimes < 0; } set { RepeatTimes = value ? -1 : 0; } }
+            public bool IsPingPong { get; set; }
+            public bool IsReverse { get; set; }
+            public bool IsForward { get { return !IsReverse; } set { IsReverse = !value; } }
 
-            public int CurrentSpriteID {
-                get { return Frames[CurrentFrameIndex]; }
-                set {
-                    for (int i = 0; i < Frames.Length; i++) {
-                        if (Frames[i] == value)
-                            CurrentFrameIndex = i;
+            public int TotalDuration {
+                get {
+                    int total = 0;
+                    foreach (int t in Durations) {
+                        total += t;
                     }
+
+                    return total;
                 }
+            }
+
+            public void Reset() {
+                HasEnded = false;
+                CurrentFrameIndex = IsReverse ? Frames.Length - 1 : 0;
+                TimesPlayed = 0;
+            }
+
+            public void NextFrame() {
+                if (HasEnded) {
+                    return;
+                }
+
+                if ((IsReverse && CurrentFrameIndex - 1 < 0) || (IsForward && CurrentFrameIndex + 1 == Frames.Length)) {
+                    if (IsPingPong) {
+                        IsReverse = !IsReverse;
+                    } 
+                    
+                    if (IsLooping || TimesPlayed < RepeatTimes) {
+                        CurrentFrameIndex = IsReverse ? Frames.Length - 1 : 0;
+                        TimesPlayed++;
+                        _onEnd?.Invoke();
+                    } else {
+                        HasEnded = true;
+                        return;
+                    }
+                } else {
+                    CurrentFrameIndex = IsReverse ? CurrentFrameIndex - 1 : CurrentFrameIndex + 1;
+                    _onChangeFrame?.Invoke();
+                }
+            }
+
+            public Track Repeat(int times = -1) {
+                RepeatTimes = times;
+                return this;
+            }
+
+            public Track Loop() {
+                IsLooping = true;
+                return this;
+            }
+
+            public Track PingPong() {
+                IsPingPong = true;
+                RepeatTimes = 1;
+                return this;
+            }
+
+            public Track Reverse() {
+                IsReverse = true;
+                CurrentFrameIndex = Frames.Length - 1;
+                return this;
+            }
+
+            public Track Forward() {
+                IsForward = true;
+                CurrentFrameIndex = 0;
+                return this;
+            }
+
+            public Track OnEnd(Action onEnd) {
+                _onEnd += onEnd;
+                return this;
+            }
+
+            public Track OnChangeFrame(Action onChangeFrame) {
+                _onChangeFrame += onChangeFrame;
+                return this;
             }
         }
 
