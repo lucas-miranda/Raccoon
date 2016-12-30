be824287d53078dfa36349da7825a56fbf613640
Lucas Miranda <miranda.a.lucas@gmail.com>
2016-12-29 23:02:26 -0200

    Added Random and Range

---
+++ Raccoon/Util/Random.cs
@@ -0,0 +1,65 @@
+﻿using Raccoon.Graphics;
+
+namespace Raccoon.Util {
+    public class Random {
+        private static Random _instance = new Random();
+
+        private System.Random _rand;
+
+        private Random() {
+            _rand = new System.Random();
+        }
+
+        private Random(int seed) {
+            _rand = new System.Random(seed);
+        }
+
+        public static void Bytes(byte[] buffer) {
+            _instance._rand.NextBytes(buffer);
+        }
+
+        public static int Integer() {
+            return _instance._rand.Next();
+        }
+
+        public static int Integer(int min, int max) {
+            return _instance._rand.Next(min, max + 1);
+        }
+
+        public static float Single() {
+            return (float) Double();
+        }
+
+        public static float Single(float min, float max) {
+            return (float) Double(min, max);
+        }
+
+        public static double Double() {
+            return _instance._rand.NextDouble();
+        }
+
+        public static double Double(double min, double max) {
+            return min + Double() * (max - min);
+        }
+
+        public static Color Color() {
+            return new Color(Integer(0, 255), Integer(0, 255), Integer(0, 255));
+        }
+
+        public static Vector2 Vector2() {
+            return new Vector2((float) (Double() * 2 - 1), (float) (Double() * 2 - 1));
+        }
+
+        public static Vector2 Vector2(Rectangle area) {
+            return new Vector2(Integer((int) area.Left, (int) area.Right), Integer((int) area.Top, (int) area.Bottom));
+        }
+
+        public static Direction Direction() {
+            return (Direction) (1 << Integer(0, 3));
+        }
+
+        public static void SetSeed(int seed) {
+            _instance._rand = new System.Random(seed);
+        }
+    }
+}
---
+++ Raccoon/Util/Range.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Raccoon.Util {
+    public class Range {
+        #region Private Members
+
+        private float _min, _max;
+
+        #endregion Private Members
+
+        #region Constructors
+
+        public Range() {
+            _min = _max = 0;
+        }
+
+        public Range(float min, float max) {
+            if (min > max) throw new ArgumentException("Invalid interval, 'max' must be greater than 'min'");
+            Min = min;
+            Max = max;
+        }
+
+        public Range(Range range) {
+            Min = range.Min;
+            Max = range.Max;
+        }
+
+        #endregion Constructors
+
+        #region Public Properties
+
+        public float Length { get { return Max - Min; } }
+
+        public float Min {
+            get {
+                return _min;
+            }
+
+            set {
+                if (value > _max) throw new ArgumentException("Value must be less than 'Max'");
+
+                _min = value;
+            }
+        }
+
+        public float Max {
+            get {
+                return _max;
+            }
+
+            set {
+                if (value < _min) throw new ArgumentException("Value must be greater than 'Min'");
+
+                _max = value;
+            }
+        }
+
+        #endregion Public Properties
+
+        #region Public Static Methods
+
+        public static Range Union(Range rangeA, Range rangeB) {
+            return new Range(Math.Min(rangeA.Min, rangeB.Min), Math.Max(rangeA.Max, rangeB.Max));
+        }
+
+        #endregion Public Static Methods
+
+        #region Public Methods
+
+        public bool Overlaps(Range range) {
+            return !(Min > range.Max || range.Min > Max);
+        }
+
+        public bool Overlaps(Range range, out float amount) {
+            if (Overlaps(range)) {
+                amount = Math.Min(Max, range.Max) - Math.Max(Min, range.Min);
+                return true;
+            }
+
+            amount = 0;
+            return false;
+        }
+        
+        public bool Overlaps(Range range, out Range overlappedRange) {
+            if (Overlaps(range)) {
+                overlappedRange = new Range(Math.Max(Min, range.Min), Math.Min(Max, range.Max));
+                return true;
+            }
+
+            overlappedRange = new Range();
+            return false;
+        }
+
+        public float Clamp(float value) {
+            return Math.Clamp(value, Min, Max);
+        }
+
+        public float Distance(Range range) {
+            if (Overlaps(range)) {
+                return 0;
+            }
+
+            return range.Min > Max ? range.Min - Max : Min - range.Max;
+        }
+
+        public bool Contains(float value) {
+            return value >= Min && value <= Max;
+        }
+
+        public bool ContainsExclusive(float value) {
+            return value > Min && value < Max;
+        }
+
+        public void Union(Range range) {
+            Min = Math.Min(Min, range.Min);
+            Max = Math.Max(Max, range.Max);
+        }
+
+        #endregion Public Methods
+    }
+}
