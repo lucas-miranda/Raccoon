f8de1370caa4277bd128f1f28ac880bcde3505b2
Lucas Miranda <miranda.a.lucas@gmail.com>
2016-12-29 23:09:00 -0200

    Added Graphics.Line and Graphics.Polygon

---
+++ Raccoon/Graphics/Drawables/Primitives/Line.cs
@@ -0,0 +1,32 @@
+﻿namespace Raccoon.Graphics.Primitives {
+    public class Line : Graphic {
+        private Vector2 _to;
+
+        public Line(Vector2 from, Vector2 to, Color color) {
+            From = from;
+            To = To;
+            Color = color;
+        }
+
+        public Line(Vector2 length, Color color) {
+            From = Vector2.Zero;
+            To = length;
+            Color = color;
+        }
+
+        public Vector2 From { get { return Position - Origin; } set { Position = value + Origin; } }
+        public Vector2 To { get { return From + _to; } set { _to = value - From; } }
+        public Raccoon.Line Equation { get { return new Raccoon.Line(From, To); } }
+
+        public override void Render(Vector2 position, float rotation) {
+            Game.Instance.Core.BasicEffect.CurrentTechnique.Passes[0].Apply();
+            Game.Instance.Core.GraphicsDevice.DrawUserPrimitives(Microsoft.Xna.Framework.Graphics.PrimitiveType.LineList,
+                new Microsoft.Xna.Framework.Graphics.VertexPositionColor[2] {
+                    new Microsoft.Xna.Framework.Graphics.VertexPositionColor(new Microsoft.Xna.Framework.Vector3(position.X - Origin.X, position.Y - Origin.Y, 0), Color),
+                    new Microsoft.Xna.Framework.Graphics.VertexPositionColor(new Microsoft.Xna.Framework.Vector3(position.X - Origin.X + _to.X, position.Y - Origin.Y + _to.Y, 0), Color)
+                }, 0, 1);
+        }
+
+        public override void Dispose() { }
+    }
+}
---
+++ Raccoon/Graphics/Drawables/Primitives/Polygon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Raccoon.Graphics.Primitives {
+    public class Polygon : Graphic {
+        public Polygon(Raccoon.Polygon polygon, Color color) {
+            Shape = polygon;
+            Color = color;
+        }
+
+        public Polygon(IEnumerable<Vector2> points, Color color) : this(new Raccoon.Polygon(points), color) { }
+
+        public Raccoon.Polygon Shape { get; set; }
+
+        public override void Render(Vector2 position, float rotation) {
+            Game.Instance.Core.BasicEffect.CurrentTechnique.Passes[0].Apply();
+            for (int i = 0; i < Shape.VertexCount; i++) {
+                Vector2 vertex = Shape[i], nextVertex = Shape[(i + 1) % Shape.VertexCount];
+                Game.Instance.Core.GraphicsDevice.DrawUserPrimitives(Microsoft.Xna.Framework.Graphics.PrimitiveType.LineList,
+                    new Microsoft.Xna.Framework.Graphics.VertexPositionColor[2] {
+                        new Microsoft.Xna.Framework.Graphics.VertexPositionColor(new Microsoft.Xna.Framework.Vector3(position.X - Origin.X + vertex.X, position.Y - Origin.Y + vertex.Y, 0), Color),
+                        new Microsoft.Xna.Framework.Graphics.VertexPositionColor(new Microsoft.Xna.Framework.Vector3(position.X - Origin.X + nextVertex.X, position.Y - Origin.Y + nextVertex.Y, 0), Color)
+                    }, 0, 1);
+            }
+        }
+
+        public override void Dispose() { }
+    }
+}
