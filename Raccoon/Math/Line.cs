ef7f6a6f7468617086e1775e6d405788577c0742
Lucas Miranda <miranda.a.lucas@gmail.com>
2016-12-29 23:05:39 -0200

    Added Line and Polygon

---
+++ Raccoon/Math/Line.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Raccoon {
+    public struct Line {
+        public Line(Vector2 pointA, Vector2 pointB) {
+            PointA = pointA;
+            PointB = pointB;
+            Slope = pointB.X - pointA.X != 0 ? (pointB.Y - pointA.Y) / (pointB.X - pointA.X) : float.PositiveInfinity;
+            YIntercept = PointB.X - PointA.X != 0 ? (PointA.Y * PointB.X - PointA.X * PointB.Y) / (PointB.X - PointA.X) : float.PositiveInfinity;
+            A = PointA.Y - PointB.Y;
+            B = PointB.X - PointA.X;
+            C = (PointA.X - PointB.X) * PointA.Y + (PointB.Y - PointA.Y) * PointB.Y;
+        }
+
+        public Line(float a, float b, float c) {
+            A = a;
+            B = b;
+            C = c;
+            Slope = B == 0 ? float.PositiveInfinity : - A / B;
+            YIntercept = B == 0 ? float.PositiveInfinity : - C / B;
+            float XIntercept = - C / A;
+            PointA = new Vector2(XIntercept, B == 0 ? 0 : YIntercept);
+            PointB = new Vector2(float.IsInfinity(Slope) ? XIntercept : XIntercept + 1, B == 0 ? 1 : Slope + YIntercept);
+        }
+
+        public Line(float slope, float yIntercept) {
+            if (float.IsInfinity(slope)) throw new ArgumentException("Slope value can't be infinity", "slope");
+
+            Slope = slope;
+            YIntercept = yIntercept;
+            PointA = new Vector2(0, YIntercept);
+            PointB = new Vector2(1, Slope + YIntercept);
+            A = PointA.Y - PointB.Y;
+            B = PointB.X - PointA.X;
+            C = (PointA.X - PointB.X) * PointA.Y + (PointB.Y - PointA.Y) * PointB.Y;
+        }
+
+        public Vector2 PointA { get; }
+        public Vector2 PointB { get; }
+        public float Length { get { return Util.Math.Distance(PointA, PointB); } }
+        public float LengthSquared { get { return Util.Math.DistanceSquared(PointA, PointB); } }
+        public float Slope { get; }
+        public float YIntercept { get; }
+        public float M { get { return Slope; } }
+        public float A { get; }
+        public float B { get; }
+        public float C { get; }
+
+        public Vector2 GetPoint(float x) {
+            return new Vector2(x, B == 0 ? float.PositiveInfinity : Slope * x + YIntercept);
+        }
+    }
+}
---
+++ Raccoon/Math/Polygon.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Raccoon {
+    public class Polygon : IEnumerable {
+        private List<Vector2> _vertices = new List<Vector2>();
+
+        public Polygon() { }
+
+        public Polygon(IEnumerable<Vector2> points) {
+            AddVertices(points);
+        }
+
+        public Polygon(params Vector2[] points) {
+            AddVertices(points);
+        }
+
+        public int VertexCount { get { return _vertices.Count; } }
+
+        public Vector2 this [int index] {
+            get {
+                return _vertices[index];
+            }
+
+            set {
+                _vertices[index] = value;
+            }
+        }
+
+        public static Polygon RotateAround(Polygon polygon, float degrees, Vector2 origin) {
+            List<Vector2> _rotatedVertices = new List<Vector2>(polygon._vertices.Count);
+            foreach (Vector2 vertex in polygon._vertices) {
+                _rotatedVertices.Add(Util.Math.RotateAround(vertex, origin, degrees));
+            }
+
+            return new Polygon(_rotatedVertices);
+        }
+
+        public static Polygon Rotate(Polygon polygon, float degrees) {
+            return RotateAround(polygon, degrees, polygon[0]);
+        }
+
+        public void AddVertex(Vector2 vertex) {
+            _vertices.Add(vertex);
+        }
+
+        public void AddVertices(IEnumerable<Vector2> vertex) {
+            _vertices.AddRange(vertex);
+        }
+
+        public void RemoveVertex(Vector2 vertex) {
+            _vertices.Remove(vertex);
+        }
+
+        public void RemoveVertex(int index) {
+            _vertices.RemoveAt(index);
+        }
+
+        public void Translate(Vector2 dist) {
+            for (int i = 0; i < VertexCount; i++) {
+                _vertices[i] += dist;
+            }
+        }
+
+        public void Translate(float x, float y) {
+            Translate(new Vector2(x, y));
+        }
+
+        public void RotateAround(float degrees, Vector2 origin) {
+            if (degrees == 0) {
+                return;
+            }
+
+            List<Vector2> _rotatedVertices = new List<Vector2>(_vertices.Count);
+            foreach (Vector2 vertex in _vertices) {
+                _rotatedVertices.Add(Util.Math.RotateAround(vertex, origin, degrees));
+            }
+
+            _vertices = _rotatedVertices;
+        }
+
+        public void Rotate(float degrees) {
+            RotateAround(degrees, _vertices[0]);
+        }
+
+        public float[] Projection(Vector2 axis) {
+            return Util.Math.Projection(axis, _vertices);
+        }
+
+        public Polygon Clone() {
+            Polygon clone = new Polygon();
+            clone.AddVertices(_vertices);
+            return clone;
+        }
+
+        public IEnumerator GetEnumerator() {
+            return _vertices.GetEnumerator();
+        }
+
+        public override string ToString() {
+            string s = "[Polygon | ";
+            foreach (Vector2 vertex in _vertices) {
+                s +=  vertex + " ";
+            }
+
+            return s + "]";
+        }
+    }
+}
