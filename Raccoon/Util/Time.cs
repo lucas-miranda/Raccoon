73ed9fe6dcea56e149c55d5e4e454b6217aeebea
Lucas Miranda <miranda.a.lucas@gmail.com>
2016-12-29 23:02:08 -0200

    Small Util.Time fix

--- Raccoon/Util/Time.cs
+++ Raccoon/Util/Time.cs
@@ -4,10 +4,10 @@
         public const float SecToMili = 1000f;
 
         public static long Ticks { get { return Game.Instance.Core.Time.Ticks; } }
-        public static int Milliseconds { get { return Game.Instance.Core.Time.Milliseconds; } }
-        public static int Seconds { get { return Game.Instance.Core.Time.Seconds; } }
-        public static int Minutes { get { return Game.Instance.Core.Time.Minutes; } }
-        public static int Hours { get { return Game.Instance.Core.Time.Hours; } }
-        public static int Days { get { return Game.Instance.Core.Time.Days; } }
+        public static double Milliseconds { get { return Game.Instance.Core.Time.TotalMilliseconds; } }
+        public static double Seconds { get { return Game.Instance.Core.Time.TotalSeconds; } }
+        public static double Minutes { get { return Game.Instance.Core.Time.TotalMinutes; } }
+        public static double Hours { get { return Game.Instance.Core.Time.TotalHours; } }
+        public static double Days { get { return Game.Instance.Core.Time.TotalDays; } }
     }
 }
