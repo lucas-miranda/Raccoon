276ab04092a721a5c2d6559538db97f390c195a1
Lucas Miranda <miranda.a.lucas@gmail.com>
2017-02-10 16:27:26 -0200

    Math/Polygon improvements
    Methods to reflect points, triangulate (using ear clipping), detect if is convex and to calculate convex components in a concave polygon

--- Raccoon/Math/Polygon.cs
+++ Raccoon/Math/Polygon.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 namespace Raccoon {
     public class Polygon : IEnumerable {
         private List<Vector2> _vertices = new List<Vector2>();
+        private List<Polygon> _convexComponents = new List<Polygon>();
+        private bool _needRecalculateComponents = false;
 
         public Polygon() { }
 
@@ -15,7 +17,19 @@ namespace Raccoon {
             AddVertices(points);
         }
 
+        public Polygon(Polygon polygon) {
+            _vertices.AddRange(polygon._vertices);
+
+            foreach (Polygon convexComponent in polygon.GetConvexComponents()) {
+                _convexComponents.Add(new Polygon(convexComponent._vertices));
+            }
+
+            IsConvex = polygon.IsConvex;
+            _needRecalculateComponents = polygon._needRecalculateComponents;
+        }
+
         public int VertexCount { get { return _vertices.Count; } }
+        public bool IsConvex { get; private set; }
 
         public Vector2 this [int index] {
             get {
@@ -24,6 +38,7 @@ namespace Raccoon {
 
             set {
                 _vertices[index] = value;
+                Verify();
             }
         }
 
@@ -42,10 +57,12 @@ namespace Raccoon {
 
         public void AddVertex(Vector2 vertex) {
             _vertices.Add(vertex);
+            Verify();
         }
 
         public void AddVertices(IEnumerable<Vector2> vertex) {
             _vertices.AddRange(vertex);
+            Verify();
         }
 
         public void RemoveVertex(Vector2 vertex) {
@@ -83,14 +100,86 @@ namespace Raccoon {
             RotateAround(degrees, _vertices[0]);
         }
 
+        public void Reflect(Vector2 axis) {
+            for (int i = 0; i < VertexCount; i++) {
+                Vector2 vertex = _vertices[i];
+                _vertices[i] = new Vector2(2 * axis.X - vertex.X, 2 * axis.Y - vertex.Y);
+            }
+        }
+
+        public void ReflectHorizontal(float axis) {
+            for (int i = 0; i < VertexCount; i++) {
+                Vector2 vertex = _vertices[i];
+                _vertices[i] = new Vector2(2 * axis - vertex.X, vertex.Y);
+            }
+        }
+
+        public void ReflectVertical(float axis) {
+            for (int i = 0; i < VertexCount; i++) {
+                Vector2 vertex = _vertices[i];
+                _vertices[i] = new Vector2(vertex.X, 2 * axis - vertex.Y);
+            }
+        }
+
         public float[] Projection(Vector2 axis) {
             return Util.Math.Projection(axis, _vertices);
         }
 
+        public List<Polygon> GetConvexComponents() {
+            if (_needRecalculateComponents) {
+                RecalculateComponents();
+                _needRecalculateComponents = false;
+            }
+
+            return _convexComponents;
+        }
+
+        public List<Polygon> Triangulate() {
+            // triangulate using ear clipping
+            List<Polygon> triangles = new List<Polygon>();
+            Stack<LinkedListNode<Vector2>> ears = new Stack<LinkedListNode<Vector2>>();
+
+            LinkedList<Vector2> vertices = new LinkedList<Vector2>(_vertices);
+            LinkedListNode<Vector2> current = vertices.First;
+            while (current != null) {
+                LinkedListNode<Vector2> previous = current.Previous == null ? vertices.Last : current.Previous,
+                                        next = current.Next == null ? vertices.First : current.Next;
+
+                // is current vertex an ear?
+                if (IsEar(previous.Value, current.Value, next.Value)) {
+                    ears.Push(current);
+                }
+
+                current = current.Next;
+            }
+
+            while (vertices.Count > 0) {
+                current = ears.Pop();
+
+                LinkedListNode<Vector2> previous = current.Previous == null ? vertices.Last : current.Previous,
+                                        next = current.Next == null ? vertices.First : current.Next;
+
+                triangles.Add(new Polygon(previous.Value, current.Value, next.Value));
+                vertices.Remove(current);
+
+                if (previous != next) {
+                    // is previous vertex an ear?
+                    if (IsEar((previous.Previous == null ? vertices.Last : previous.Previous).Value, previous.Value, next.Value)) {
+                        ears.Push(previous);
+                    }
+
+                    // is next vertex an ear?
+                    if (IsEar(previous.Value, next.Value, (next.Next == null ? vertices.First : next.Next).Value)) {
+                        ears.Push(next);
+                    }
+                }
+            }
+
+            return triangles;
+        }
+
         public Polygon Clone() {
-            Polygon clone = new Polygon();
-            clone.AddVertices(_vertices);
-            return clone;
+            return new Polygon(this);
         }
 
         public IEnumerator GetEnumerator() {
@@ -104,6 +193,101 @@ namespace Raccoon {
             }
 
             return s + "]";
+        }
+
+        private void Verify() {
+            if (VertexCount < 3) {
+                return;
+            }
+
+            IsConvex = true;
+
+            // algorithm to determine if it's convex: http://stackoverflow.com/a/1881201
+            Vector2 previous = _vertices[_vertices.Count - 1], center = _vertices[0], next = _vertices[1];
+            int sign = System.Math.Sign((center.X - previous.X) * (next.Y - center.Y) - (center.Y - previous.Y) * (next.X - center.X));
+
+            previous = center;
+            for (int i = 1; i < VertexCount; i++) {
+                center = _vertices[i];
+                next = _vertices[(i + 1) % _vertices.Count];
+                if (sign != System.Math.Sign((center.X - previous.X) * (next.Y - center.Y) - (center.Y - previous.Y) * (next.X - center.X))) {
+                    IsConvex = false;
+                    break;
+                }
+
+                previous = center;
+            }
+
+            _needRecalculateComponents = true;
+        }
+
+        private void RecalculateComponents() {
+            _convexComponents.Clear();
+            if (IsConvex) {
+                return;
+            }
+
+            List<Polygon> triangles = Triangulate();
+
+            Polygon currentComponent;
+            List<int> trianglesUsed = new List<int>();
+            int verticesConnecting = 0, cadidateVertexId = -1;
+            while (triangles.Count > 0) {
+                currentComponent = new Polygon(triangles[0]);
+                trianglesUsed.Add(0);
+
+                for (int i = 1; i < triangles.Count; i++) {
+                    Polygon triangle = triangles[i];
+                    verticesConnecting = 0;
+
+                    for (int j = 0; j < triangle.VertexCount; j++) {
+                        for (int k = 0; k < currentComponent.VertexCount; k++) {
+                            if (currentComponent[k] == triangle[j]) {
+                                verticesConnecting++;
+                            } else {
+                                cadidateVertexId = j;
+                            }
+                        }
+                    }
+
+                    if (verticesConnecting == 2) {
+                        Vector2 candidateVertex = triangle[cadidateVertexId];
+                        currentComponent.AddVertex(candidateVertex);
+
+                        if (currentComponent.IsConvex) {
+                            trianglesUsed.Add(i);
+                        } else {
+                            currentComponent.RemoveVertex(candidateVertex);
+                        }
+                    }
+                }
+
+                foreach (int triangleId in trianglesUsed) {
+                    triangles.RemoveAt(triangleId);
+                }
+                trianglesUsed.Clear();
+
+                _convexComponents.Add(currentComponent);
+            }
+        }
+
+        private bool IsEar(Vector2 previous, Vector2 center, Vector2 next) {
+            if (System.Math.Abs(Util.Math.Angle(previous, center, next)) >= 180) {
+                return false;
+            }
+
+            // test if some other points lies inside (if so triangle isn't a ear)
+            foreach (Vector2 point in _vertices) {
+                if (point == previous || point == center || point == next) {
+                    continue;
+                }
+
+                if (Util.Math.IsPointInsideTriangle(previous, center, next, point)) {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
