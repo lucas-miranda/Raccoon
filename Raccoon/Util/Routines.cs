0c14f5777a540c026a4c1956847cb8a9fa680797
Lucas Miranda <miranda.a.lucas@gmail.com>
2017-11-28 22:41:03 -0200

    Added Routines

---
+++ Raccoon/Util/Routines.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+
+namespace Raccoon.Util {
+    public static class Routines {
+        public static IEnumerator WaitFor(Func<bool> condition) {
+            while (!condition()) {
+                yield return null;
+            }
+        }
+    }
+}
