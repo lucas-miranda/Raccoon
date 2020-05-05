75e278ec407a990d3e50d404eca7c3ab59c4ddd5
Lucas Miranda <miranda.a.lucas@gmail.com>
2020-05-04 21:15:03 -0300

    Update to Raccoon.csproj cs entries organization

--- Raccoon/Graphics/Shaders/iShaderVertexColor.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Xna.Framework;
-
-namespace Raccoon.Graphics {
-    public interface IShaderVertexColor {
-        Color DiffuseColor { get; set; }
-        float Alpha { get; set; }
-    }
-}
