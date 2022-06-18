87163d5f6384d9bab02e2a9c4668e689f4660fe9
Lucas Miranda <miranda.a.lucas@gmail.com>
2022-06-18 16:37:01 -0300

    feat: improvements to Location

--- Raccoon/Math/Location.cs
+++ Raccoon/Math/Location.cs
@@ -1,11 +1,24 @@
 
 namespace Raccoon {
     public struct Location {
+        public static readonly Location Zero = new Location(0, 0),
+                                        Unit = new Location(1, 1),
+                                        UnitX = new Location(1, 0),
+                                        UnitY = new Location(0, 1);
+
         public int X, Y;
 
         public Location(int x, int y) {
             X = x;
             Y = y;
+        }
+
+        public Location PerpendicularCCW() {
+            return new Location(Y, -X);
+        }
+
+        public Location PerpendicularCW() {
+            return new Location(-Y, X);
         }
 
         public override int GetHashCode() {
@@ -25,6 +38,38 @@ namespace Raccoon {
 
         public override string ToString() {
             return $"{X}, {Y}";
+        }
+
+        public static bool operator ==(Location l, Location r) {
+            return l.X == r.X && l.Y == r.Y;
+        }
+
+        public static bool operator !=(Location l, Location r) {
+            return l.X != r.X || l.Y != r.Y;
+        }
+
+        public static Location operator -(Location l) {
+            return new Location(-l.X, -l.Y);
+        }
+
+        public static Location operator +(Location l, Location r) {
+            return new Location(l.X + r.X, l.Y + r.Y);
+        }
+
+        public static Location operator -(Location l, Location r) {
+            return new Location(l.X - r.X, l.Y - r.Y);
+        }
+
+        public static Location operator *(Location l, Location r) {
+            return new Location(l.X * r.X, l.Y * r.Y);
+        }
+
+        public static Location operator *(Location l, int v) {
+            return new Location(l.X * v, l.Y * v);
+        }
+
+        public static Location operator *(int v, Location r) {
+            return new Location(v * r.X, v * r.Y);
         }
     }
 }
