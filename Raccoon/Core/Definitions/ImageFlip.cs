8ddcdc7598a26e959b95085ff60100c4fa1e9d0f
Lucas Miranda <miranda.a.lucas@gmail.com>
2022-08-08 20:22:24 -0300

    feat: add ImageFlip extensions

--- Raccoon/Core/Definitions/ImageFlip.cs
+++ Raccoon/Core/Definitions/ImageFlip.cs
@@ -8,4 +8,22 @@ namespace Raccoon {
         Vertical = 1 << 1,
         Both = Horizontal | Vertical
     }
+
+    public static class ImageFlipExtensions {
+        public static bool IsHorizontal(this ImageFlip flip) {
+            return (flip & ImageFlip.Horizontal) == ImageFlip.Horizontal;
+        }
+
+        public static bool IsVertical(this ImageFlip flip) {
+            return (flip & ImageFlip.Vertical) == ImageFlip.Vertical;
+        }
+
+        public static bool IsBoth(this ImageFlip flip) {
+            return flip == ImageFlip.Both;
+        }
+
+        public static bool IsNone(this ImageFlip flip) {
+            return flip == ImageFlip.None;
+        }
+    }
 }
