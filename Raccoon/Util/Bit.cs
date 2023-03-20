a18d04bd0096c4fb7a39c61299cf91a04de03987
Lucas Miranda <miranda.a.lucas@gmail.com>
2023-03-20 13:34:00 -0300

    feat: add more methods to Bit

--- Raccoon/Util/Bit.cs
+++ Raccoon/Util/Bit.cs
@@ -16,12 +16,32 @@
             flags &= ~((uint) 1 << bit);
         }
 
+        public static bool HasSet(ref long flags, int bit) {
+            return (flags & (1L << bit)) != 0;
+        }
+
+        public static bool HasSet(ref ulong flags, int bit) {
+            return (flags & (1UL << bit)) != 0;
+        }
+
         public static bool HasSet(ref int flags, int bit) {
             return (flags & (1 << bit)) != 0;
         }
 
         public static bool HasSet(ref uint flags, int bit) {
-            return (flags & ((uint) 1 << bit)) != 0;
+            return (flags & (1U << bit)) != 0;
+        }
+
+        public static bool HasSet(ref short flags, int bit) {
+            return (flags & (1 << bit)) != 0;
+        }
+
+        public static bool HasSet(ref ushort flags, int bit) {
+            return (flags & (1 << bit)) != 0;
+        }
+
+        public static bool HasSet(ref byte flags, int bit) {
+            return (flags & (1 << bit)) != 0;
         }
     }
 }
